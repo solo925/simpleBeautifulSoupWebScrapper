7fe22715e3ae96219d9bb46bfa3c31bd9e86b05e
davinci <davinci@localhost.localdomain>
2024-08-17 12:26:31 +0300

    updated webscrap.py script

--- webscrap.py
+++ webscrap.py
@@ -1,1 +1,17 @@
 from bs4 import BeautifulSoup
+import requests
+
+url = "https://www.newegg.com/Processors-Desktops/SubCategory/ID-343/Page-1"
+
+result= requests.get(url).text
+# result.text
+doc = BeautifulSoup(result,"html.parser")
+prices = doc.find_all(string="$")
+
+parent = prices[0].parent
+
+strong = parent.strong.string
+
+print(strong)
+
+# print(doc.prettify())
